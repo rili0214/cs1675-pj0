81145ebba40254055584941419a007144b4c36ba
Yuming Xie <62629102+rili0214@users.noreply.github.com>
2025-09-23 01:41:42 -0400

    Update tcp_server.rs

--- src/tcp_server.rs
+++ src/tcp_server.rs
@@ -1,25 +1,53 @@
 use std::{
-    net::{SocketAddrV4, TcpStream},
+    net::{SocketAddrV4, TcpStream, TcpListener},
+    thread,
+};
+
+use crate::{
+    chunked_tcp_stream::ChunkedTcpStream,
+    protocol::work_request::ClientWorkPacketConn,
+    protocol::work_response::ServerWorkPacketConn,
 };
 
 // TODO: Students will have to fill in tcp_server, handle_conn and use the code
 // they implemented in recv_work_msg and do_work
 
 pub fn tcp_server(addr: SocketAddrV4) {
-    // TODO: Students will have to write this code.
-    // There's nothing special here except the server should listen for new
-    // client connections and then spin off a new thread to handle that
-    // connection.
-    unimplemented!()
+    let listener = TcpListener::bind(addr).expect("failed to bind TCP listener");
+    println!("server listening on {}", addr);
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => { thread::spawn(move || handle_conn(stream)); }
+            Err(e) => { eprintln!("Incoming Error: {}", e); }
+        }
+    }
 }
 
 fn handle_conn(stream: TcpStream) {
-    // TODO: Students will have to write this code.
-    // NOTE: It might be helpful to look at protocol.rs first. You'll probably
-    // be implementing that alongside this function.
-    //
-    // This function handles one client connection. It receives a
-    // ClientWorkPacket, does work using ClientWorkPacket::do_work which returns
-    // a ServerWorkPacket, then sends this ServerWorkPacket back to the client.
-    unimplemented!()
+    let stream_clone = stream.try_clone().unwrap();
+    let mut client_conn = ClientWorkPacketConn::new(ChunkedTcpStream::new(stream_clone));
+    let mut server_conn = ServerWorkPacketConn::new(ChunkedTcpStream::new(stream));
+
+    loop {
+        let msg = match client_conn.recv_work_msg() {
+            Ok(msg) => msg,
+            Err(e) => {
+                println!("recv_work_msg Error: {}", e);
+                break;
+            }
+        };
+
+        let reply = msg.do_work();
+
+        match server_conn.send_work_msg(reply) {
+            Ok(()) => {
+                continue;
+            }
+            Err(e) => {
+                eprintln!("send_work_msg error: {}", e);
+                break;
+            }
+        }
+    }
 }
